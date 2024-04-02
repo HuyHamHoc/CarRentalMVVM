6ea58165acd5f07678e4468cf176a795029bb27e
HuyITLord <vohuy19120@gmail.com>
2024-04-02 09:17:28 +0700

    done splash screen

--- app/src/main/java/com/carrentalapp/mvvm/activity/SplashActivity.kt
+++ app/src/main/java/com/carrentalapp/mvvm/activity/SplashActivity.kt
@@ -1,24 +1,18 @@
 package com.carrentalapp.mvvm.activity
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import android.view.View
-import android.view.Window
 import android.view.WindowManager
 import android.view.animation.AnimationUtils
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.appcompat.app.AppCompatActivity
 import com.carrentalapp.mvvm.R
 import com.carrentalapp.mvvm.databinding.ActivitySplashBinding
-import com.carrentalapp.mvvm.databinding.ActivitySplashSecondBinding
 
 class SplashActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySplashBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
-//        supportActionBar?.hide()
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -27,9 +21,6 @@ class SplashActivity : AppCompatActivity() {
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
         )
-
-        val sideAnimation = AnimationUtils.loadAnimation(this,R.anim.slide)
-        binding.imgView.startAnimation(sideAnimation)
 
         Handler().postDelayed({
             startActivity(Intent(this,SplashSecondActivity::class.java))
--- app/src/main/java/com/carrentalapp/mvvm/activity/SplashSecondActivity.kt
+++ app/src/main/java/com/carrentalapp/mvvm/activity/SplashSecondActivity.kt
@@ -3,18 +3,13 @@ package com.carrentalapp.mvvm.activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.view.WindowManager.LayoutParams
-import com.carrentalapp.mvvm.R
-import com.carrentalapp.mvvm.databinding.ActivitySplashBinding
 import com.carrentalapp.mvvm.databinding.ActivitySplashSecondBinding
 
 class SplashSecondActivity : AppCompatActivity() {
-    private lateinit var binding : ActivitySplashSecondBinding
+    private lateinit var binding: ActivitySplashSecondBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
-//        supportActionBar?.hide()
         binding = ActivitySplashSecondBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -25,7 +20,7 @@ class SplashSecondActivity : AppCompatActivity() {
         )
 
         binding.btnGet.setOnClickListener {
-            val intent = Intent(this,SignInActivity::class.java)
+            val intent = Intent(this, SignInActivity::class.java)
             startActivity(intent)
         }
     }
