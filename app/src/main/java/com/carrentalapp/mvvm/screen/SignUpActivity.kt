fe1892b8ebffc66be0df20bfe4b1e460bf48a820
HuyITLord <vohuy19120@gmail.com>
2024-04-04 15:38:33 +0700

    fix string signup screen

--- app/src/main/java/com/carrentalapp/mvvm/screen/SignUpActivity.kt
+++ app/src/main/java/com/carrentalapp/mvvm/screen/SignUpActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import com.carrentalapp.mvvm.R
 import com.carrentalapp.mvvm.databinding.ActivitySignUpBinding
 import com.carrentalapp.mvvm.utils.Constants
 import com.carrentalapp.mvvm.utils.togglePasswordVisibility
@@ -32,11 +33,11 @@ class SignUpActivity : AppCompatActivity() {
                 val fullName = s.toString().trim()
 
                 if (fullName.isEmpty()) {
-                    binding.edtFullname.error = "Name cannot be empty"
+                    binding.edtFullname.error = getString(R.string.username_cannot_empty)
                 } else if (!fullName.matches(Constants.FULLNAME_ALPHABETIC_REGEX)) {
-                    binding.edtFullname.error = "Only characters from a-z or A-Z are allowed"
+                    binding.edtFullname.error = getString(R.string.full_name_error)
                 } else if (!fullName.matches(Constants.USERNAME_LENGTH_REGEX)) {
-                    binding.edtFullname.error = "Name must be 8-20 characters long"
+                    binding.edtFullname.error = getString(R.string.full_name_error)
                 } else {
                     binding.edtFullname.error = null
                 }
@@ -56,11 +57,11 @@ class SignUpActivity : AppCompatActivity() {
                 val userInput = s.toString().trim()
 
                 if (userInput.isEmpty()) {
-                    binding.edtUsername.error = "Cannot be empty"
+                    binding.edtUsername.error = getString(R.string.username_cannot_empty)
                 } else if (!userInput.matches(Constants.USERNAME_REGEX)) {
-                    binding.edtUsername.error = "Can only contain letters and numbers"
+                    binding.edtUsername.error = getString(R.string.username_letters_and_numbers)
                 }else if (!userInput.matches(Constants.USERNAME_LENGTH_REGEX)) {
-                    binding.edtUsername.error = "Must be 8-20 characters long"
+                    binding.edtUsername.error = getString(R.string.username_between_8_20_characters_long)
                 } else {
                     binding.edtUsername.error = null
                 }
@@ -78,13 +79,13 @@ class SignUpActivity : AppCompatActivity() {
                 val password = s.toString()
 
                 if (password.isEmpty()) {
-                    binding.edtPassword.error = "Password cannot be empty"
+                    binding.edtPassword.error = getString(R.string.password_cannot_empty)
                 } else if (password.contains(Constants.PASSWORD_CONTAINS_SPECIAL_CHARACTERS_REGEX)) {
-                    binding.edtPassword.error = "Password contains invalid characters"
+                    binding.edtPassword.error = getString(R.string.password_characters)
                 } else if (!password.matches(Constants.PASSWORD_LENGTH_REGEX)) {
-                    binding.edtPassword.error = "Password must be 8-20 characters long"
+                    binding.edtPassword.error = getString(R.string.password_between_8_20_characters_long)
                 } else if (!password.contains(Constants.PASSWORD_SPECIAL_CHARACTERS_REGEX)) {
-                    binding.edtPassword.error = "Password must contain at least one special character: @, #, &, \$, !"
+                    binding.edtPassword.error = getString(R.string.password_must_character)
                 } else {
                     binding.edtPassword.error = null
                 }
