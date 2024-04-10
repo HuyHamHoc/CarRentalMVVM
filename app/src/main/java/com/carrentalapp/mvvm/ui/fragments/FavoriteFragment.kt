a5945cc06f6eac569940af5b36ca95b79d271116
HuyITLord <vohuy19120@gmail.com>
2024-04-10 13:47:18 +0700

    push code create favorite screen

--- app/src/main/java/com/carrentalapp/mvvm/ui/fragments/FavoriteFragment.kt
+++ app/src/main/java/com/carrentalapp/mvvm/ui/fragments/FavoriteFragment.kt
@@ -5,22 +5,50 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.carrentalapp.mvvm.R
-
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.carrentalapp.mvvm.adapter.CarsFavoritesAdapter
+import com.carrentalapp.mvvm.databinding.FragmentFavoriteBinding
+import com.carrentalapp.mvvm.model.Cars
 
 class FavoriteFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private var _binding: FragmentFavoriteBinding? = null
 
-    }
+    private val binding get() = _binding!!
+
+    private val carsFavoriteAdapter = CarsFavoritesAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorite, container, false)
+        _binding = FragmentFavoriteBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupRecyclerView()
+
+
+        val cars = List(100) { index ->
+            Cars(
+                name = "TOYOTA", price = 300
+            )
+        }
+        carsFavoriteAdapter.setCars(cars)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun setupRecyclerView() {
+        binding.recyclerView.apply {
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+            adapter = carsFavoriteAdapter
+        }
+    }
 }
